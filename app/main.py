8ac9dde647dfa1bc95fde5b3562d81658d891eac
Gleb4ik2001 <g.kalashnikovvv@gmail.com>
2026-07-23 10:44:33 +0500

    feat: added main.py

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from app.database import init_db
+from app.web.routes import router as web_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    
+    yield
+    
+app = FastAPI(title="Сервис скачивания и анализа файлов", lifespan=lifespan)
+
+app.include_router(web_router)
